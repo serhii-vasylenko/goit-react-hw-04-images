590da88d3fdf1a637d5a1ffbfca35fd1d5d636dd
Jerrssey <fankton@gmail.com>
2023-03-16 15:30:36 +0200

    task 04/3 hooked

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,102 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
-import { toast } from 'react-toastify';
 
-import { fetchImages } from 'services/image-api';
 
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
-import { Loader } from 'components/Loader/Loader';
-import { Button } from 'components/Button/Button';
-import { Modal } from 'components/Modal/Modal';
 
+export const Dashboard = () => {
+  const [query, setQuery] = useState('');
 
-export class Dashboard extends Component {
-  state = {
-    query: '',
-    images: [],
-    page: 1,
-    loading: false,
-    totalHits: 0,
-    showModal: false,
-    modalImage: '',
+  const hadleFormSubmit = query => {
+    setQuery(query);
   };
+  
 
-  componentDidUpdate(prevProps, prevState) {
-      if (
-      this.state.query !== prevState.query ||
-      this.state.page !== prevState.page
-    ) {
-      this.fetch();
-    }
-
-    if (this.state.query !== prevState.query) {
-      setTimeout(() => {
-        toast.info(`We found ${this.state.totalHits} images!`)
-      },500)
-    }
-  }
-
-  hadleFormSubmit = query => {
-    this.setState({ query, images: [], page: 1 });
-  };
-
-  async fetch() {
-    try {
-      this.setState({ loading: true });
-      const { page, query } = this.state;
-      const { hits, totalHits } = await fetchImages(query, page);
-
-      this.setState(({ images }) => ({
-        images: [...images, ...hits],
-        totalHits,
-      }));
-      
-    } catch (error) {
-    } finally {
-      this.setState({ loading: false });
-    }
-  }
-
-  handleLoadMoreClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-  };
-
-  onShowModal = item => {
-    this.setState({ modalImage: item, showModal: this.toggleModal });
-  };
-
-  render() {
-    const { images, loading, totalHits, page, showModal, modalImage } = this.state;
-    const totalPages = Math.ceil(totalHits / 12);
-    return (
-      <div className='Dashboard'>
-        <Searchbar onSubmit={this.hadleFormSubmit} />
-        <ImageGallery images={images} onShowModal={this.onShowModal} />
-        {loading && <Loader />}
-        {images.length > 0 && page < totalPages && (
-          <Button text="Load More" onClick={this.handleLoadMoreClick} />
-        )}
-        {showModal && <Modal onClose={this.toggleModal} item={modalImage}/>}
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Dashboard">
+      <Searchbar onSubmit={hadleFormSubmit} />
+      <ImageGallery  query={query} />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </div>
+  );
+};
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,23 +1,91 @@
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+
+import { fetchImages } from 'services/image-api';
 
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import {List} from 'components/ImageGallery/ImageGallery.styled'
+import { List } from 'components/ImageGallery/ImageGallery.styled';
+import { Loader } from 'components/Loader/Loader';
+import { Button } from 'components/Button/Button';
+import { Modal } from 'components/Modal/Modal';
 
-export const ImageGallery = ({ images, onShowModal }) => {
+export const ImageGallery = ({ query }) => {
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [totalHits, setTotalHits] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImage, setModalImage] = useState('');
+
+  const totalPages = Math.ceil(totalHits / 12);
+
+  useEffect(() => {
+    setSearch(query);
+    setPage(1);
+    setImages([]);
+  }, [query]);
+
+  useEffect(() => {
+    if (!search && page === 1) {
+      return;
+    }
+    const Fetch = async () => {
+      try {
+        setLoading(true);
+
+        const { hits, totalHits } = await fetchImages(search, page);
+
+        setImages(images => [...images, ...hits]);
+        setTotalHits(totalHits);
+
+        totalHits
+          ? toast.info(`We found ${totalHits} image(s)`)
+          : toast.warning(`Oops, 0 images found`);
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
+    };
+    Fetch();
+  }, [search, page]);
+
+  const handleLoadMoreClick = () => {
+    setPage(state => state + 1);
+  };
+
+  const onShowModal = item => {
+    setModalImage(item);
+    setShowModal(toggleModal);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
-    <List>
-      {images.map(image => (
-        <ImageGalleryItem
-          item={image}
-          key={image.id}
-          onShowModal={onShowModal}
-        />
-      ))}
-    </List>
+    <>
+      {images.length > 0 && (
+        <List>
+          {images.map(image => (
+            <ImageGalleryItem
+              item={image}
+              key={image.id}
+              onShowModal={onShowModal}
+            />
+          ))}
+        </List>
+      )}
+      {loading && <Loader />}
+      {images.length > 0 && page < totalPages && (
+        <Button text="Load More" onClick={handleLoadMoreClick} />
+      )}
+      {showModal && <Modal onClose={toggleModal} item={modalImage} />}
+    </>
   );
 };
 
 ImageGallery.propTypes = {
-  images: PropTypes.array.isRequired,
-  onShowModal: PropTypes.func.isRequired,
+  query: PropTypes.string.isRequired,
 };
