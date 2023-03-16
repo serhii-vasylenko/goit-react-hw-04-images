a20359004ef6bfb9e75ed67d686d1ab258844c68
Jerrssey <fankton@gmail.com>
2023-03-16 12:33:18 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,46 +1,43 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 import { Overlay, ImageWrapper } from 'components/Modal/Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.habdleEscPress);
-  }
+export const Modal = ({ item, onClose }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', habdleEscPress);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.habdleEscPress);
-  }
+    return () => {
+      window.removeEventListener('keydown', habdleEscPress);
+    };
+  });
 
-  habdleEscPress = ({ code }) => {
+  const habdleEscPress = ({ code }) => {
     if (code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClick = ({ currentTarget, target }) => {
+  const handleBackdropClick = ({ currentTarget, target }) => {
     if (currentTarget === target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const {
-      item: { largeImageURL, tags },
-    } = this.props;
-    return createPortal(
-      <Overlay onClick={this.handleBackdropClick}>
-        <ImageWrapper>
-          <img src={largeImageURL} alt={tags} />
-        </ImageWrapper>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+  const { largeImageURL, tags } = item;
+
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      <ImageWrapper>
+        <img src={largeImageURL} alt={tags} />
+      </ImageWrapper>
+    </Overlay>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   item: PropTypes.object.isRequired,
